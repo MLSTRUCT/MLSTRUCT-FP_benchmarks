01175858652da8974d15da34e10c42a5c3b96d6c
ppizarror <pablo@ppizarror.com>
2023-01-23 09:58:52 -0300

    Fix setup

---
+++ MLStructFP_benchmarks/__init__.py
@@ -0,0 +1,17 @@
+"""
+MLSTRUCTFP BENCHMARKS
+Machine learning structural floor plan dataset (benchmarks).
+"""
+
+# Basic information
+__author__ = 'Pablo Pizarro R.'
+__description__ = 'Machine learning structural floor plan dataset benchmark'
+__keywords__ = ['ml', 'ai', 'dataset', 'calc', 'matrix analysis', 'cnn', 'structural analysis', 'structural design']
+__email__ = 'pablo@ppizarror.com'
+__version__ = '1.0.0'
+
+# URL
+__url__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP-benchmarks'
+__url_bug_tracker__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP-benchmarks/issues'
+__url_documentation__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP-benchmarks'
+__url_source_code__ = 'https://github.com/MLSTRUCT/MLSTRUCT_FP-benchmarks'
--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ Setup distribution.
 """
 
 # Library imports
+import MLStructFP_benchmarks
 from setuptools import setup, find_packages
-import MLStructFP
 
 # Load readme
 with open('README.rst') as f:
@@ -27,8 +27,8 @@ requirements_tests.extend([
 
 # Setup library
 setup(
-    author=MLStructFP.__author__,
-    author_email=MLStructFP.__email__,
+    author=MLStructFP_benchmarks.__author__,
+    author_email=MLStructFP_benchmarks.__email__,
     classifiers=[
         'Natural Language :: English',
         'Operating System :: OS Independent',
@@ -39,14 +39,14 @@ setup(
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Topic :: Scientific/Engineering :: Visualization'
     ],
-    description=MLStructFP.__description__,
+    description=MLStructFP_benchmarks.__description__,
     long_description=long_description,
     include_package_data=True,
     install_requires=requirements,
     extras_require={
         'test': requirements_tests
     },
-    keywords=MLStructFP.__keywords__,
+    keywords=MLStructFP_benchmarks.__keywords__,
     name='MLStructFP-benchmarks',
     packages=find_packages(exclude=[
         '.idea',
@@ -55,11 +55,11 @@ setup(
     ]),
     platforms=['any'],
     project_urls={
-        'Bug Tracker': MLStructFP.__url_bug_tracker__,
-        'Documentation': MLStructFP.__url_documentation__,
-        'Source Code': MLStructFP.__url_source_code__
+        'Bug Tracker': MLStructFP_benchmarks.__url_bug_tracker__,
+        'Documentation': MLStructFP_benchmarks.__url_documentation__,
+        'Source Code': MLStructFP_benchmarks.__url_source_code__
     },
     python_requires='>=3.8',
-    url=MLStructFP.__url__,
-    version=MLStructFP.__version__
+    url=MLStructFP_benchmarks.__url__,
+    version=MLStructFP_benchmarks.__version__
 )
