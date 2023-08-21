5d0690a1bcd7e2bc936754260b83e9aecf962754
Pablo Pizarro R <pablo@ppizarror.com>
2023-08-20 22:15:41 -0400

    Delete unsafe.py

--- unsafe.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def unsafe_query(user_input):
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
-    query = "SELECT * FROM users WHERE username = '" + user_input + "';"
-    cursor.execute(query)
-    return cursor.fetchall(result)
